1a60e82026ddbf4eba2aeac38bbbdebd214638b5
leomonde <git@gitlab.com>
2024-02-23 20:21:17 -0300

    force volvo

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -349,10 +349,9 @@ class Controls:
 
     for i, pandaState in enumerate(self.sm['pandaStates']):
       # All pandas must match the list of safetyConfigs, and if outside this list, must be silent or noOutput
-      pandaState.safetyModel = "volvo"
       if i < len(self.CP.safetyConfigs):
-        safety_mismatch = pandaState.safetyModel != self.CP.safetyConfigs[i].safetyModel or \
-                          pandaState.safetyParam != self.CP.safetyConfigs[i].safetyParam or \
+        safety_mismatch = #pandaState.safetyModel != self.CP.safetyConfigs[i].safetyModel or \
+        safety_mismatch = pandaState.safetyParam != self.CP.safetyConfigs[i].safetyParam or \
                           pandaState.alternativeExperience != self.CP.alternativeExperience
         #vp
         #print("pandaState.safetyModel:{} self.CP.safetyConfigs[i].safetyModel:{}".format(pandaState.safetyModel, self.CP.safetyConfigs[i].safetyModel))
